84b0f49a3f41e3f7f3de0c7c2487995c11d984fc
longcon123 <44388919+longcon123@users.noreply.github.com>
2020-10-03 22:09:43 +0700

    Update DictionaryCommandline.java

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,24 +1,22 @@
-import java.io.IOException;
+//import java.io.IOException;
 
-public class DictionaryCommandline {
-    static Dictionary newDic = new Dictionary();
-    static DictionaryManagements dicMan = new DictionaryManagements(newDic);
+public class DictionaryCommandline extends DictionaryManagements{
     public static void showAllWords() {
         System.out.println("No ||English    |Vietnamese");
-        for(int i = 0; i < dicMan.dic.getSize(); i++) {
-            String vi = dicMan.dic.getWords(i).getWord_target();
+        for(int i = 0; i < getSize(); i++) {
+            String vi = getWord(i);
             System.out.print(i+1 + "  |"+vi);
             for(int j = vi.length(); j < 15; j++) {
                 System.out.print(" ");
             }
-            System.out.println("|"+dicMan.dic.getWords(i).getWord_explain());
+            System.out.println("|" + getWordDefi(i));
         }
     }
     public static void dictionaryBasic() {
-        dicMan.insertFromCommandline();
+        insertFromCommandline();
         showAllWords();
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         //dicMan.insertFromFile();
         dictionaryBasic();
     }
