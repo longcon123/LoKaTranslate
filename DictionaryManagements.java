e7d4973b572f72393ae00ad172f75ae432173e14
longcon123 <44388919+longcon123@users.noreply.github.com>
2020-10-03 21:54:47 +0700

    Update DictionaryManagements.java

--- DictionaryManagements.java
+++ DictionaryManagements.java
@@ -1,8 +1,13 @@
+import java.io.*;
 import java.util.Scanner;
 public class DictionaryManagements {
     static Scanner sc = new Scanner(System.in);
     static int stt = 0;
-    public static void insertFromCommandline(Dictionary dic) {
+    static Dictionary dic = new Dictionary();
+    public DictionaryManagements(Dictionary dic) {
+        this.dic = dic;
+    }
+    public static void insertFromCommandline() {
         long n = sc.nextLong();
         sc.nextLine();
         for(int i = stt; i < stt+n; i++) {
@@ -11,6 +16,15 @@ public class DictionaryManagements {
             Word word = new Word(eWord, vExpl);
             dic.addWord(word);
             //stt++;
+        }
+    }
+    public static void insertFromFile() throws IOException {
+        FileReader fis = new FileReader("D:\\BTL_Dictionary\\src\\main\\resources\\E_V.txt");
+        BufferedReader br = new BufferedReader(fis);
+        String line;
+        int i = 0;
+        while ((line = br.readLine()) != null) {
+            //read file
         }
     }
 }
